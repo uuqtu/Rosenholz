e28bf8d9bde1f04a7aeed78a4c59ad401f521471
Frédéric Jean-Claude Joseph Arnolin <frederic.arnolin@siemens.com>
2022-09-17 20:24:09 +0200

    FolderComboBox entfernt.

--- Rosenholz.UserControls/AUExplorer.xaml.cs
+++ Rosenholz.UserControls/AUExplorer.xaml.cs
@@ -26,22 +26,12 @@ namespace Rosenholz.UserControls
     public partial class AUExplorer : UserControl
     {
 
-        private string _currentFolder = "";
-
-        public string CurrentFolder
-        {
-            get { return _currentFolder; }
-            set
-            {
-                _currentFolder = value;
-                OnPropertyChanged(nameof(CurrentFolder));
-            }
-        }
         public AUExplorer()
         {
             InitializeComponent();
             // InitializeFileSystemObjects();
             FolderExplorerView.CurrentFolder = @"D:\OneDrive - Siemens AG\unix";
+
         }
 
         private void FolderView_OnFileOpen(object sender, FolderExplorer.FListView.FileOpenEventArgs e)
@@ -51,114 +41,10 @@ namespace Rosenholz.UserControls
 
         public void OnCurrentFolderChanged(string path)
         {
-            CurrentFolder = path;
+           
+
         }
-        //#region Events
-
-        //private void FileSystemObject_AfterExplore(object sender, System.EventArgs e)
-        //{
-        //    Cursor = Cursors.Arrow;
-        //}
-
-        //private void FileSystemObject_BeforeExplore(object sender, System.EventArgs e)
-        //{
-        //    Cursor = Cursors.Wait;
-        //}
-
-        //#endregion
-
-        //#region Methods
-
-        //public void InitializeFileSystemObjects(Model.AUReference reference)
-        //{
-        //    string folderpath = System.IO.Path.Combine(Settings.Settings.Instance.BasePath,
-        //                                     Rosenholz.Model.FolderManager.Instance.GetRelativeAUFolderLocation(reference.AUReferenceString));
-
-        //    FileSystemObjectInfo.FolderPath = folderpath;
-
-        //    AuReferenceGridView.Items.Clear();
-        //    var drives = DriveInfo.GetDrives();
-        //    DriveInfo
-        //        .GetDrives()
-        //        //.Where(x => x.VolumeLabel == "B:\\" || x.VolumeLabel == "C:\\" || x.VolumeLabel == "D:\\")
-        //        .ToList()
-        //        .ForEach(drive =>
-        //        {
-        //            var fileSystemObject = new FileSystemObjectInfo(drive);
-        //            fileSystemObject.BeforeExplore += FileSystemObject_BeforeExplore;
-        //            fileSystemObject.AfterExplore += FileSystemObject_AfterExplore;
-        //            AuReferenceGridView.Items.Add(fileSystemObject);
-        //        });
-        //    //PreSelect(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
-        //    PreSelect(folderpath);
-
-        //}
-
-        //private void PreSelect(string path)
-        //{
-        //    if (!Directory.Exists(path))
-        //    {
-        //        return;
-        //    }
-        //    var driveFileSystemObjectInfo = GetDriveFileSystemObjectInfo(path);
-        //    driveFileSystemObjectInfo.IsExpanded = true;
-        //    PreSelect(driveFileSystemObjectInfo, path);
-        //}
-
-        //private void PreSelect(FileSystemObjectInfo fileSystemObjectInfo,
-        //    string path)
-        //{
-        //    foreach (var childFileSystemObjectInfo in fileSystemObjectInfo.Children)
-        //    {
-        //        var isParentPath = IsParentPath(path, childFileSystemObjectInfo.FileSystemInfo.FullName);
-        //        if (isParentPath)
-        //        {
-        //            if (string.Equals(childFileSystemObjectInfo.FileSystemInfo.FullName, path))
-        //            {
-        //                /* We found the item for pre-selection */
-        //            }
-        //            else
-        //            {
-        //                childFileSystemObjectInfo.IsExpanded = true;
-        //                PreSelect(childFileSystemObjectInfo, path);
-        //            }
-        //        }
-        //    }
-        //}
-
-        //#endregion
-
-        //#region Helpers
-
-        //private FileSystemObjectInfo GetDriveFileSystemObjectInfo(string path)
-        //{
-        //    var directory = new DirectoryInfo(path);
-        //    var drive = DriveInfo
-        //        .GetDrives()
-        //        .Where(d => d.RootDirectory.FullName == directory.Root.FullName)
-        //        .FirstOrDefault();
-        //    return GetDriveFileSystemObjectInfo(drive);
-        //}
-
-        //private FileSystemObjectInfo GetDriveFileSystemObjectInfo(DriveInfo drive)
-        //{
-        //    foreach (var fso in AuReferenceGridView.Items.OfType<FileSystemObjectInfo>())
-        //    {
-        //        if (fso.FileSystemInfo.FullName == drive.RootDirectory.FullName)
-        //        {
-        //            return fso;
-        //        }
-        //    }
-        //    return null;
-        //}
-
-        //private bool IsParentPath(string path,
-        //    string targetPath)
-        //{
-        //    return path.StartsWith(targetPath);
-        //}
-
-        //#endregion
+       
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged(string propertyName)
