66f1b52b764fc20e056524d058606bfb542fd300
z0035hes <frederic.arnolin@siemens.com>
2022-10-15 23:24:12 +0200

    Fixed crude Settings.

--- Rosenholz.Windows/Settings/InitialSettings.xaml.cs
+++ Rosenholz.Windows/Settings/InitialSettings.xaml.cs
@@ -26,11 +26,19 @@ namespace Rosenholz.Windows
     {
         private ObservableCollection<string> _iniFiles;
         public ObservableCollection<string> IniFiles { get { return _iniFiles; } set { _iniFiles = value; OnPropertyChanged(nameof(IniFiles)); } }
+        private string _selectedIniPath;
+        public string SelectedIniPath
+        {
+            get { return _selectedIniPath; }
+            set
+            {
+                _selectedIniPath = value;
+                OnPropertyChanged(nameof(SelectedIniPath));
+            }
+        }
         public InitialSettings()
         {
             GetIniFiles();
-            SelectedIniFileItem = IniFiles[0];
-
             InitializeComponent();
             DataContext = this;
         }
@@ -40,92 +48,143 @@ namespace Rosenholz.Windows
         {
             var dir = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.ini");
 
-            IniFiles = new ObservableCollection<string>(dir.ToList());
+
+            IniFiles = new ObservableCollection<string>(dir.ToList().Where(s => !s.Contains("settings.ini")));
         }
 
+        private string _storageBaseLocation;
+        public string StorageBaseLocation
+        {
+            get { return _storageBaseLocation; }
+            set
+            {
+                _storageBaseLocation = value;
+                OnPropertyChanged(nameof(StorageBaseLocation));
+            }
+        }
 
+        private string _position;
         public string Position
         {
             get
             {
-                return Settings.Settings.Instance.Position;
+                return _position;
             }
             set
             {
-                Settings.Settings.Instance.Position = value;
+                _position = value;
                 OnPropertyChanged(nameof(Position));
             }
         }
 
-        public string F22Location
+        private string _f22SubLocation;
+        public string F22SubLocation
         {
-            get { return Settings.Settings.Instance.F22Location; }
+            get { return _f22SubLocation; }
             set
             {
-                Settings.Settings.Instance.F22Location = value;
-                OnPropertyChanged(nameof(F22Location));
+                _f22SubLocation = value;
+                OnPropertyChanged(nameof(F22SubLocation));
             }
         }
 
-        public string F16Location
+
+        private string _f16SubLocation;
+        public string F16SubLocation
         {
-            get { return Settings.Settings.Instance.F16Location; }
+            get { return _f16SubLocation; }
             set
             {
-                Settings.Settings.Instance.F16Location = value;
-                OnPropertyChanged(nameof(F16Location));
+                _f16SubLocation = value;
+                OnPropertyChanged(nameof(F16SubLocation));
             }
         }
 
-        public string BasePath
+
+
+
+        private string _taskSubLocation;
+        public string TaskSubLocation
         {
-            get { return Settings.Settings.Instance.BasePath; }
+            get { return _taskSubLocation; }
             set
             {
-                Settings.Settings.Instance.BasePath = value;
-                OnPropertyChanged(nameof(BasePath));
+                _taskSubLocation = value;
+                OnPropertyChanged(nameof(TaskSubLocation));
             }
         }
 
-        public string FileTypeFilters
+        private string _aUSubLocation;
+        public string AUSubLocation
         {
-            get { return Settings.Settings.Instance.FileTypeFilters; }
+            get { return _aUSubLocation; }
             set
             {
-                Settings.Settings.Instance.FileTypeFilters = value;
-                OnPropertyChanged(nameof(FileTypeFilters));
+                _aUSubLocation = value;
+                OnPropertyChanged(nameof(AUSubLocation));
             }
         }
 
-        public string TaskLocation
+        #region Filenames
+
+        private string _f22FileName;
+        public string F22FileName
         {
-            get { return Settings.Settings.Instance.TaskLocation; }
+            get { return _f22FileName; }
             set
             {
-                Settings.Settings.Instance.TaskLocation = value;
-                OnPropertyChanged(nameof(TaskLocation));
+                _f22FileName = value;
+                OnPropertyChanged(nameof(F22FileName));
             }
         }
 
-        public string TaskItemLocation
+        private string _f16FileName;
+        public string F16FileName
         {
-            get { return Settings.Settings.Instance.TaskItemLocation; }
+            get { return _f16FileName; }
             set
             {
-                Settings.Settings.Instance.TaskItemLocation = value;
-                OnPropertyChanged(nameof(TaskItemLocation));
+                _f16FileName = value;
+                OnPropertyChanged(nameof(F16FileName));
             }
         }
 
-        public string AppBaseLocation
+        private string _tasksFileName;
+        public string TasksFileName
         {
-            get { return Settings.Settings.Instance.AppBaseLocation; }
+            get { return _tasksFileName; }
             set
             {
-                Settings.Settings.Instance.AppBaseLocation = value;
-                OnPropertyChanged(nameof(AppBaseLocation));
+                _tasksFileName = value;
+                OnPropertyChanged(nameof(TasksFileName));
             }
         }
+
+
+        private string _taskItemsFileName;
+        public string TaskItemsFileName
+        {
+            get { return _taskItemsFileName; }
+            set
+            {
+                _taskItemsFileName = value;
+                OnPropertyChanged(nameof(TaskItemsFileName));
+            }
+        }
+
+        private string _taskLinkFileName;
+        public string TaskLinkFileName
+        {
+            get { return _taskLinkFileName; }
+            set
+            {
+                _taskLinkFileName = value;
+                OnPropertyChanged(nameof(TaskLinkFileName));
+            }
+        }
+
+        #endregion
+
 
 
 
@@ -146,8 +205,6 @@ namespace Rosenholz.Windows
         }
 
 
-
-
         private bool CanEcexuteCloseSettings()
         {
             return true;
@@ -158,6 +215,8 @@ namespace Rosenholz.Windows
             this.Close();
         }
 
+
+        #region CreateNewSettingsCommand
         private RelayCommand _createNewSettingsCommand;
         public RelayCommand CreateNewSettingsCommand
         {
@@ -167,54 +226,177 @@ namespace Rosenholz.Windows
                 {
                     _createNewSettingsCommand = new RelayCommand(
                         (parameter) => CreateNewSettingsExecute(),
-                        (parameter) => CanEcexuteCreateNewSettings()
+                        (parameter) => true
                     );
                 }
                 return _createNewSettingsCommand;
             }
         }
-
-
-
-
-        private bool CanEcexuteCreateNewSettings()
-        {
-            return true;
-        }
-
         public void CreateNewSettingsExecute()
         {
             InputBox box = new InputBox("Wie soll die SettingsDatei heißen?");
             box.ShowDialog();
             var str = box.InputString;
             IniFile var = new IniFile(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{str}.ini"));
+            var.IniWriteValue("Admin", nameof(Position), 2);
+            var.IniWriteValue("Storage", nameof(StorageBaseLocation), System.IO.Path.Combine(Environment.ExpandEnvironmentVariables("%desktop%"), "MFS"));
+            var.IniWriteValue("FilePath", nameof(F22SubLocation), System.IO.Path.Combine(Environment.ExpandEnvironmentVariables("%desktop%"), "MFS", "ZVK"));
+            var.IniWriteValue("FilePath", nameof(F16SubLocation), System.IO.Path.Combine(Environment.ExpandEnvironmentVariables("%desktop%"), "MFS", "ZPK"));
+            var.IniWriteValue("FilePath", nameof(TaskSubLocation), System.IO.Path.Combine(Environment.ExpandEnvironmentVariables("%desktop%"), "MFS", "ZTV"));
+            var.IniWriteValue("FilePath", nameof(AUSubLocation), System.IO.Path.Combine(Environment.ExpandEnvironmentVariables("%desktop%"), "MFS", "ZAV"));
+            var.IniWriteValue("FileName", nameof(F22FileName), "f22.db");
+            var.IniWriteValue("FileName", nameof(F16FileName), "f16.db");
+            var.IniWriteValue("FileName", nameof(TasksFileName), "tasks.db");
+            var.IniWriteValue("FileName", nameof(TaskItemsFileName), "taskitems.db");
+            var.IniWriteValue("FileName", nameof(TaskLinkFileName), "linkedtaskitems.db");
+
             GetIniFiles();
         }
+        #endregion
 
-
-        private string _selectedIniFileItem;
-        public string SelectedIniFileItem
+        #region LoadSettingsCommand
+        private RelayCommand _loadSettingsCommand;
+        public RelayCommand LoadSettingsCommand
         {
             get
             {
-                return _selectedIniFileItem;
-            }
-            set
-            {
-                var param = _selectedIniFileItem = value;
-                Settings.Settings.Location = param;
-                OnPropertyChanged(nameof(SelectedIniFileItem));
-                Position = Position;
-                F22Location = F22Location;
-                F16Location = F16Location;
-                BasePath = BasePath;
-                FileTypeFilters = FileTypeFilters;
-                TaskLocation = TaskLocation;
-                TaskItemLocation = TaskItemLocation;
-                AppBaseLocation = AppBaseLocation;
+                if (_loadSettingsCommand == null)
+                {
+                    _loadSettingsCommand = new RelayCommand(
+                        (parameter) => LoadSettingsCommandExecute(parameter),
+                        (parameter) => !String.IsNullOrWhiteSpace(SelectedIniPath)
+                    );
+                }
+                return _loadSettingsCommand;
             }
         }
+        public void LoadSettingsCommandExecute(object parameter)
+        {
+            if (parameter is string)
+            {
+                IniFile var = new IniFile(parameter.ToString());
+                Position = var.IniReadValue("Admin", nameof(Position));
+                StorageBaseLocation = var.IniReadValue("Storage", nameof(StorageBaseLocation));
+                F22SubLocation = var.IniReadValue("FilePath", nameof(F22SubLocation));
+                F16SubLocation = var.IniReadValue("FilePath", nameof(F16SubLocation));
+                TaskSubLocation = var.IniReadValue("FilePath", nameof(TaskSubLocation));
+                AUSubLocation = var.IniReadValue("FilePath", nameof(AUSubLocation));
+                F22FileName = var.IniReadValue("FileName", nameof(F22FileName));
+                F16FileName = var.IniReadValue("FileName", nameof(F16FileName));
+                TasksFileName = var.IniReadValue("FileName", nameof(TasksFileName));
+                TaskItemsFileName = var.IniReadValue("FileName", nameof(TaskItemsFileName));
+                TaskLinkFileName = var.IniReadValue("FileName", nameof(TaskLinkFileName));
+                GetIniFiles();
+            }
+        }
+        #endregion
 
+        #region SaveSettingsCommand
+        private RelayCommand _saveSettingsCommand;
+        public RelayCommand SaveSettingsCommand
+        {
+            get
+            {
+                if (_saveSettingsCommand == null)
+                {
+                    _saveSettingsCommand = new RelayCommand(
+                        (parameter) => SaveSettingsCommandExecute(parameter),
+                        (parameter) => !String.IsNullOrWhiteSpace(SelectedIniPath)
+                    );
+                }
+                return _saveSettingsCommand;
+            }
+        }
+        public void SaveSettingsCommandExecute(object parameter)
+        {
+            if (parameter is string)
+            {
+                IniFile var = new IniFile(parameter.ToString());
+                var.IniWriteValue("Admin", "Position", Position);
+                var.IniWriteValue("Organization", "F22SubLocation", F22SubLocation);
+                var.IniWriteValue("Organization", "F16SubLocation", F16SubLocation);
+                var.IniWriteValue("Organization", "AUSubLocation", AUSubLocation);
+                var.IniWriteValue("Organization", "TaskSubLocation", TaskSubLocation);
+                var.IniWriteValue("Storage", "StorageBaseLocation", StorageBaseLocation);
+                GetIniFiles();
+            }
+        }
+        #endregion
+
+        #region SetSettingsAndOpenCommand
+        private RelayCommand _setSettingsAndOpenCommand;
+        public RelayCommand SetSettingsAndOpenCommand
+        {
+            get
+            {
+                if (_setSettingsAndOpenCommand == null)
+                {
+                    _setSettingsAndOpenCommand = new RelayCommand(
+                        (parameter) => SetSettingsAndOpenCommandExecute(parameter),
+                        (parameter) => !String.IsNullOrWhiteSpace(SelectedIniPath)
+                    );
+                }
+                return _setSettingsAndOpenCommand;
+            }
+        }
+        public void SetSettingsAndOpenCommandExecute(object parameter)
+        {
+            if (parameter is string)
+            {
+                IniFile var = new IniFile(parameter.ToString());
+                var.IniWriteValue("Admin", nameof(Position), Position);
+                var.IniWriteValue("Storage", nameof(StorageBaseLocation), StorageBaseLocation);
+                var.IniWriteValue("FilePath", nameof(F22SubLocation), F22SubLocation);
+                var.IniWriteValue("FilePath", nameof(F16SubLocation), F16SubLocation);
+                var.IniWriteValue("FilePath", nameof(TaskSubLocation), TaskSubLocation);
+                var.IniWriteValue("FilePath", nameof(AUSubLocation), AUSubLocation);
+                var.IniWriteValue("FileName", nameof(F22FileName), F22FileName);
+                var.IniWriteValue("FileName", nameof(F16FileName), F16FileName);
+                var.IniWriteValue("FileName", nameof(TasksFileName), TasksFileName);
+                var.IniWriteValue("FileName", nameof(TaskItemsFileName), TaskItemsFileName);
+                var.IniWriteValue("FileName", nameof(TaskLinkFileName), TaskLinkFileName);
+
+                File.Delete("settings.ini");
+
+                IniFile settings = new IniFile(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "settings.ini"));
+                settings.IniWriteValue("Admin", nameof(Position), Position);
+                settings.IniWriteValue("Storage", nameof(StorageBaseLocation), StorageBaseLocation);
+                settings.IniWriteValue("FilePath", nameof(F22SubLocation), F22SubLocation);
+                settings.IniWriteValue("FilePath", nameof(F16SubLocation), F16SubLocation);
+                settings.IniWriteValue("FilePath", nameof(TaskSubLocation), TaskSubLocation);
+                settings.IniWriteValue("FilePath", nameof(AUSubLocation), AUSubLocation);
+                settings.IniWriteValue("FileName", nameof(F22FileName), F22FileName);
+                settings.IniWriteValue("FileName", nameof(F16FileName), F16FileName);
+                settings.IniWriteValue("FileName", nameof(TasksFileName), TasksFileName);
+                settings.IniWriteValue("FileName", nameof(TaskItemsFileName), TaskItemsFileName);
+                settings.IniWriteValue("FileName", nameof(TaskLinkFileName), TaskLinkFileName);
+
+                this.Close();
+            }
+        }
+        #endregion
+
+        #region OpenDefaultCommand
+        private RelayCommand _penDefaultCommand;
+        public RelayCommand OpenDefaultCommand
+        {
+            get
+            {
+                if (_penDefaultCommand == null)
+                {
+                    _penDefaultCommand = new RelayCommand(
+                        (parameter) => OpenDefaultCommandExecute(parameter),
+                        (parameter) => File.Exists("settings.ini")
+                    );
+                }
+                return _penDefaultCommand;
+            }
+        }
+        public void OpenDefaultCommandExecute(object parameter)
+        {
+            this.Close();
+        }
+        #endregion
 
 
 
@@ -223,6 +405,11 @@ namespace Rosenholz.Windows
         private void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadSettingsCommandExecute(SelectedIniPath);
         }
     }
 }
